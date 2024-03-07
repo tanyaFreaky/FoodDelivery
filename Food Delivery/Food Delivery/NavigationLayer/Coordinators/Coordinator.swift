4cd6e8680fe88f8e60b90b864efae95439a28c00
Таня <tanyafreaky@mail.ru>
2024-03-07 14:18:32 +0300

    Added coordinators protocol

---
+++ Food Delivery/Food Delivery/NavigationLayer/Coordinators/Coordinator.swift
@@ -0,0 +1,41 @@
+//
+//  Coordinator.swift
+//  Food Delivery
+//
+//  Created by Tanya on 07.03.2024.
+//
+
+import UIKit
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case home
+    case order
+    case list
+    case profile
+}
+
+protocol CoordinatorProtocol: AnyObject {
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+    var navigationController: UINavigationController? { get set }
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    
+    func start()
+    func finish()
+}
+
+extension CoordinatorProtocol {
+    func addChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators.append(childCoordinator)
+    }
+    func removeChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators = childCoordinators.filter{ $0 !== childCoordinator }
+    }
+}
+
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinators: CoordinatorProtocol)
+}

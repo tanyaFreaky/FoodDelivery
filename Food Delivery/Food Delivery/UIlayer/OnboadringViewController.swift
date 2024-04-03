b496d3c641140b86e6d4f84a0e8837215e8d11db
Таня <tanyafreaky@mail.ru>
2024-04-03 20:08:33 +0300

    added Keyboard-observers

--- Food Delivery/Food Delivery/UIlayer/OnboadringViewController.swift
+++ Food Delivery/Food Delivery/UIlayer/OnboadringViewController.swift
@@ -100,7 +100,9 @@ private extension OnboardingViewController {
     func setupButton() {
         view.addSubview(bottomButton)
         bottomButton.translatesAutoresizingMaskIntoConstraints = false
-        bottomButton.action = buttonPressed
+        bottomButton.action = { [weak self] in
+            self?.buttonPressed()
+        }
         bottomButton.scheme = .grey
         
         NSLayoutConstraint.activate([
